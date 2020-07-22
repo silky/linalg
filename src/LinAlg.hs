d72ccc6f4e1829eb30c499328cc66794b785c1ac
Conal Elliott <conal@conal.net>
2020-07-22 12:32:46 -0700

    getting started

---
+++ src/LinAlg.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE TypeOperators #-}
+
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
+
+-- | Linear algebra for the post-Fortran age
+
+module LinAlg where
+
+import Control.Category
+
+-- | A g × f "matrix" of morphisms from a to b
+newtype Matrix f g k a b = M (g (f (a `k` b)))
+
+instance Category (Matrix f g k) where
+  id = M (...)
+
+-- Hm. I think I need to compose Matrix g h k b c with Matrix f g k a b to get a
+-- Matrix f h k a b.
