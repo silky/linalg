8b7fbc66ab65b2b9ac2812d09d68e5227097f1f3
Conal Elliott <conal@conal.net>
2020-07-22 12:33:54 -0700

    comment

--- src/LinAlg.hs
+++ src/LinAlg.hs
@@ -12,8 +12,8 @@ import Control.Category
 -- | A g × f "matrix" of morphisms from a to b
 newtype Matrix f g k a b = M (g (f (a `k` b)))
 
-instance Category (Matrix f g k) where
-  id = M (...)
+-- instance Category (Matrix f g k) where
+--   id = M (...)
 
 -- Hm. I think I need to compose Matrix g h k b c with Matrix f g k a b to get a
 -- Matrix f h k a b.
