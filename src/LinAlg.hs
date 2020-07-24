a6c96d00a648a6e9215c363e0e381ca2a4a50f20
Conal Elliott <conal@conal.net>
2020-07-24 01:43:14 -0700

    tidy

--- src/LinAlg.hs
+++ src/LinAlg.hs
@@ -42,7 +42,7 @@ instance Additive s => Additive (L f g s) where
   ForkL ms + Fork ms' = ForkL (liftR2 (+) ms ms')
 
 unforkL :: Representable h => L f (h :.: g) s -> h (L f g s)
-unforkL Zero = pureRep Zero
+unforkL Zero       = pureRep Zero
 unforkL (ForkL ms) = ms
 unforkL (JoinL ms) = fmap JoinL (distribute (fmap unforkL ms))
 
@@ -57,7 +57,7 @@ fmap JoinL (distrib (fmap unforkL ms)) :: h (L (k :.: f) g s)
 #endif
 
 unjoinL :: Representable h => L (h :.: f) g s -> h (L f g s)
-unjoinL Zero = pureRep Zero
+unjoinL Zero       = pureRep Zero
 unjoinL (JoinL ms) = ms
 unjoinL (ForkL ms) = fmap ForkL (distribute (fmap unjoinL ms))
 
@@ -71,35 +71,9 @@ pattern Join ms <- (unjoinL -> ms) where Join = JoinL
 
 infixr 9 .@
 (.@) :: Semiring s => L g h s -> L f g s -> L f h s
-Zero .@ _ = Zero
-_ .@ Zero = Zero
-Scale a .@ Scale b = Scale (a * b)                -- Scale denotation
-ForkL ms' .@ m = ForkL (fmap (.@ m) ms')          -- categorical product law
-m' .@ JoinL ms = JoinL (fmap (m' .@) ms)          -- categorical coproduct law
-JoinL ms' .@ Fork ms = sum (liftR2 (.@) ms' ms)   -- biproduct law
-
-#if 0
--- ForkL clause types:
-
-ForkL              ms  :: L g (k :.: h) s
-                   ms  :: k (L g h s)
-                m      :: L f g s
-       fmap (.@ m) ms  :: k (L f h s)
-ForkL (fmap (.@ m) ms) :: L f (k :.: h) s
-
--- join . fork types:
-
-JoinL ms' :: L (k :.: g) h s
-ForkL ms  :: L f (k :.: g) s
-ms' :: k (L g h s)
-ms  :: k (L f g s)
-
-liftR2 (.@) ms' ms :: k (L f h s)
-sum (liftR2 (.@) ms' ms) :: L f h s
-
-#endif
-
--- TODO: Maybe move s back to first position in L so that L s is a (functor)
--- category. On the other hand, with s last, L f g is a (representable) functor.
--- On second thought, is L f g isn't even a regular Haskell functor in a
--- semantically sensible way?
+Zero      .@ _        = Zero                       -- Zero denotation
+_         .@ Zero     = Zero                       -- linearity
+Scale a   .@ Scale b  = Scale (a * b)              -- Scale denotation
+ForkL ms' .@ m        = ForkL (fmap (.@ m) ms')    -- categorical product law
+m'        .@ JoinL ms = JoinL (fmap (m' .@) ms)    -- categorical coproduct law
+JoinL ms' .@ Fork ms  = sum (liftR2 (.@) ms' ms)   -- biproduct law
