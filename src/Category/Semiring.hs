1778c38f7fb5655075ad1d991838fdbea82b122c
Conal Elliott <conal@conal.net>
2020-08-10 10:58:18 -0700

    Category.Semiring

---
+++ src/Category/Semiring.hs
@@ -0,0 +1,20 @@
+-- | Semiring as category
+
+module Category.Semiring where
+
+import Prelude hiding ((+),(*))
+import Misc
+import Category
+
+newtype SemiringCat s a b = S s
+
+instance Semiring s => Category (SemiringCat s) where
+  type Obj' (SemiringCat s) = Unconstrained
+  id = S one
+  S t . S s = S (t * s)
+
+deriving instance Additive s => Additive (SemiringCat s a b)
+
+-- Do we want a Semiring instance?
+-- 
+-- deriving instance Semiring s => Semiring (SemiringCat s a b)
